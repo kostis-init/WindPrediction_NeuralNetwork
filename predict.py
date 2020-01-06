916a05d134e1c1717442c1ecc0620b421994c6ca
kostis <kostismich@gmail.com>
2020-01-06 20:04:58 +0200

    test code

--- predict.py
+++ predict.py
@@ -1,1 +1,14 @@
 # A
+from pandas import read_csv
+from keras.models import load_model
+
+actual_data = read_csv("input_data/actual.csv")
+nn_representations_data = read_csv("input_data/nn_representations.csv")
+print(actual_data)
+print(nn_representations_data)
+
+model = load_model('input_data/WindDenseNN.h5')
+# summarize the structure of the model
+model.summary()
+# Get the weights of the first layer of the model
+model.layers[0].get_weights()
