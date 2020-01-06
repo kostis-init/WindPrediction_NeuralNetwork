a795d76edab1f763df5ca79038df1fcc78a49a61
kostis <kostismich@gmail.com>
2020-01-07 00:15:33 +0200

    A and B almost ready

--- new_representation.py
+++ new_representation.py
@@ -1,1 +1,37 @@
-# B
+from pandas import read_csv
+import keras
+import numpy
+
+# Parse input
+
+nn_representations_data = read_csv("input_data/nn_representations.csv")
+model = keras.models.load_model('input_data/WindDenseNN.h5')
+
+# Copy the first layer of the model to a new model
+
+new_model = keras.Sequential()
+new_model.add(model.layers[0])
+
+# Keep timestamps
+
+dates = nn_representations_data.values[:,0]
+
+# Remove timestamps from data
+
+nn_representations_data = nn_representations_data.drop(nn_representations_data.columns[0], axis=1)
+
+# Execute the NN
+
+result = new_model.predict(nn_representations_data)
+
+# Write to file
+
+file = open("output_data/new_representation.csv","w")
+
+for i,row in enumerate(result):
+	file.write(str(dates[i]))
+	file.write(" ")
+	for number in row:
+		file.write(str(number))
+		file.write(" ")
+	file.write("\n")
--- predict.py
+++ predict.py
@@ -1,14 +1,61 @@
-# A
 from pandas import read_csv
 from keras.models import load_model
+import numpy
+
+# Parse input
 
 actual_data = read_csv("input_data/actual.csv")
 nn_representations_data = read_csv("input_data/nn_representations.csv")
-print(actual_data)
-print(nn_representations_data)
-
 model = load_model('input_data/WindDenseNN.h5')
-# summarize the structure of the model
-model.summary()
-# Get the weights of the first layer of the model
-model.layers[0].get_weights()
+
+# Keep timestamps
+
+dates = nn_representations_data.values[:,0]
+
+# Remove timestamps from data
+
+nn_representations_data = nn_representations_data.drop(nn_representations_data.columns[0], axis=1)
+actual_data = actual_data.drop(actual_data.columns[0], axis=1)
+
+# Execute the NN
+
+result = model.predict(nn_representations_data)
+
+# Calculate metrics
+
+mae_sum = 0
+mse_sum = 0
+mape_sum = 0
+
+counter = 0
+for i,row in enumerate(result):
+	for j,number in enumerate(row):
+		diff = numpy.abs(number - actual_data.values[i][j])
+		mae_sum += diff
+		mse_sum += diff**2
+		if actual_data.values[i][j] == 0:
+			mape_sum += diff/numpy.average(actual_data.values)
+		else:
+			mape_sum += diff/actual_data.values[i][j]
+		counter += 1
+
+
+# Write to file
+
+file = open("output_data/predicted.csv","w")
+
+file.write("MAE: ")
+file.write(str(mae_sum/counter))
+file.write(" MAPE: ")
+file.write(str(100 * mape_sum/counter))
+file.write(" MSE: ")
+file.write(str(mse_sum/counter))
+file.write("\n")
+
+for i,row in enumerate(result):
+	file.write(str(dates[i]))
+	file.write(" ")
+	for number in row:
+		file.write(str(number))
+		file.write(" ")
+	file.write("\n")
